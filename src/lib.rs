228a4828b0e3b5cdf0d115bba8ff10557cb6ee16
Muna Nwugo <munachimsonwugo@gmail.com>
2026-03-09 22:49:33 -0400

    add rust integration

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#[pyo3::pymodule]
+mod blip_mover {
+  use pyo3::prelude::*;
+
+  /// Formats the sum of two numbers as string.
+  #[pyfunction]
+  fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+  }
+}
