ea9ae22925fbedc5c2b6750470473af344bba151
Muna Nwugo <munachimsonwugo@gmail.com>
2026-03-16 22:24:10 -0400

    Blip vs blip collision detection fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #[pyo3::pymodule]
 mod blip_mover {
-    use pyo3::{prelude::*, types::PyList};
+    use pyo3::{PyResult, prelude::*, types::PyList};
 
     #[pyfunction]
     fn move_blips<'py>(
@@ -14,7 +14,7 @@ mod blip_mover {
             let change: (i32, i32) = (0, 1);
             blip.call_method1("update_pos", change)?;
         }
-        let blips_list: Bound<'py, Pylist> = blips.clone();
+        let blips_list: Bound<'py, PyList> = blips.clone();
 
         check_collisions(blips_list, width, height);
         Ok(blips.clone())
@@ -24,36 +24,33 @@ mod blip_mover {
         blips: Bound<'py, PyList>,
         width: i32,
         height: i32,
-    ) -> (bool, Vec<(PyAny, PyAny)>) {
+    ) -> PyResult<(bool, Vec<(Bound<'py, PyAny>, Bound<'py, PyAny>)>)> {
         /*Checks if any blips are currently colliding.
          * If blips are colliding, this returns true, and then a list of tuples of all colliding
          * blips*/
-        let collision_detected: bool = false;
-        let collision_pairs: Vec<PyAny, PyAny>;
+        let mut collision_detected: bool = false;
+        let mut collision_pairs: Vec<(Bound<'py, PyAny>, Bound<'py, PyAny>)> = Vec::new();
+        let blips_len = blips.len();
 
-        Python::attach(|py| {
-            let len: PyAny = PyModule::from_code(py, 
-                c"def length(ls: list) :
-                    return len(ls)", 
-                    c"length.py", 
-                    c"",
-            )?
-            .gettattr("length")?
-            .into();
-
-        blips_len: i32 = len.call1(py, (blips));
         for i in 0..blips_len {
-            current_blip: PyAny = blips.get_item(i);
-            for j: i32 in 0..blips_len {
+            let current_blip = blips.get_item(i)?;
+            let current_blip_pos = current_blip.call_method0("get_pos")?;
+
+            for j in (i + 1)..blips_len {
                 if j == i {
                     continue;
                 }
 
+                let check_blip = blips.get_item(j)?;
+                let check_blip_pos = check_blip.call_method0("get_pos")?;
 
+                if current_blip_pos.eq(check_blip_pos)? {
+                    collision_detected = true;
+                    collision_pairs.push((current_blip.clone(), check_blip));
+                }
             }
         }
 
-        })
-        return (true, blips);
+        Ok((collision_detected, collision_pairs))
     }
 }
